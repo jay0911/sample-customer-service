bb5f44073b7f0f848e1716ca60f2cf6ed7c16098
John Ryan Oliveros <johnryan.oliveros@infor.com>
2017-01-04 08:06:49 +0800

    added lambda expression and stream to list example

--- src/main/java/com/poc/CustomerSampleServiceApplication.java
+++ src/main/java/com/poc/CustomerSampleServiceApplication.java
@@ -1,5 +1,10 @@
 package com.poc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -40,5 +45,21 @@ class CustomerService{
 	@RequestMapping(value = "/postinfo", method = RequestMethod.POST)
 	public Customer postCustomerInfo(@RequestBody Customer customer){
 		return customer;
+	}
+	
+	@RequestMapping(value = "/custlist", method = RequestMethod.GET)
+	public List<Customer> customerList(){
+		List<Customer> c = new ArrayList<Customer>();
+		c.add(new Customer("jay1","cainta"));
+		c.add(new Customer("jay2","cainta"));
+		c.add(new Customer("jay3","cainta"));
+		c.add(new Customer("jay4","rizal"));
+		c.add(new Customer("jay5","rizal"));
+		c.add(new Customer("jay6","rizal"));
+
+		
+		return c.stream()
+				.filter(p -> p.getAddress().startsWith("r"))
+				.collect(Collectors.toList());
 	}
 }
